a0277d3cbcf08944d99d99d45726ec6f18bc01b7
StarkEvan <36335763+StarkEvan@users.noreply.github.com>
2019-02-18 12:17:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,13 @@ const Discord = require("discord.js");
 // this is what we're refering to. Your client.
 const client = new Discord.Client();
 
-// Here we load the config.json file that contains our token and our prefix values. 
-const config = require("./config.json");
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
   console.log(`Bot has started.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(` Kamen Rider Drive`);
+  client.user.setActivity(` Kamen Rider Chronicle`);
 });
 
 
@@ -122,4 +118,4 @@ client.on("message", async message => {
   }
 });
 
-client.login(config.token);
+client.login(process.env.BOT_TOKEN);
